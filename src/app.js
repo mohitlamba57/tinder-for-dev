c1bdac041564aceadce6ae709aec189236638995
Mohit Lamba <mohitlamba5757@gmail.com>
2024-12-21 22:12:46 +0530

    Understanding routing, params, and res query method

--- src/app.js
+++ src/app.js
@@ -2,12 +2,19 @@ import express from 'express';
 
 const app = express();
 
+app.get("/user/:id/:name", (req, res)=>{
+    console.log(req.params);
+    res.send("Posting");
+})
+
+app.get("/data", (req, res)=>{
+    console.log(req.query);
+    res.send("Posting DATA");
+})
+
 app.use("/test", (req, res)=>{
     res.send("hello");
 })
 
-app.use("/", (req, res)=>{
-    res.send("root");
-})
 
 app.listen(3000);
